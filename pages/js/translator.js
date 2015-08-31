b111aecc0fe85a8187ca1f7757410986d450211a
Ben Taylor <taylorbf@gmail.com>
2015-08-30 21:02:09 -0500

    hooking up gestures

--- pages/js/translator.js
+++ pages/js/translator.js
@@ -77,12 +77,17 @@ var Translate = {
 
 
     		// wall-specific
-    		case "gone":
-    			current.method = "kill"
-    			current.params = ""
-    			mode = "wall"
-    			// need to delete wall from local.walls too
-    			break; 
+        case "gone":
+          current.method = "kill"
+          current.params = ""
+          mode = "wall"
+          // need to delete wall from local.walls too
+          break; 
+        case "move":
+          current.method = "move"
+          current.params = words.slice(1)
+          mode = "wall"
+          break; 
     		case "blink":
     			current.method = "hide"
     			current.params = ""
@@ -116,13 +121,21 @@ var Translate = {
 
     	}
 
-		for (var key in this.shorthands) {
-			if (current.method==key) {
-				current.method = this.shorthands[key]
-			}
-		}
+  		for (var key in this.shorthands) {
+  			if (current.method==key) {
+  				current.method = this.shorthands[key]
+  			}
+  		}
 
-   		if (mode=="wall") {
+      if (current.params[0]=="by") {
+        // var a = new Gesture(box.move.bind(box),100,100)
+        if (mode=="wall") {
+          var subject = local.context
+        } else if (mode=="media") {
+          var subject = current.media
+        }
+        output = "var a = new Gesture("+subject+"."+current.method+".bind("+subject+"),"+current.params[1]+","+current.params[2]+","+current.params[3]+","+current.params[4]+","+current.params[4]+")"
+      } else if (mode=="wall") {
 
    			// output looks like: red.shapeshift("line")
    			
@@ -159,6 +172,8 @@ var Translate = {
    		current.media = current.media ? current.media : this.last.media
    		this.last = JSON.parse(JSON.stringify(current));
     	
+      console.log(output)
+
     	return output
 
     },
@@ -191,3 +206,71 @@ var Translate = {
 
     }
 }
+
+
+
+
+var Gesture = function(cb,xlow,xhigh,ylow,yhigh) {
+
+      this.buffer = [
+        [0,0],
+        [.5,0],
+        [0,.5],
+        [1,1],
+        [1,.75],
+        [0,.2]
+      ]
+    
+    
+  
+      this.bufferIndex = 0
+      this.speed = 1
+      this.limit = 100
+
+      this.cb = cb
+      this.interval;
+
+      this.scale = {
+        x: [ xlow ? xlow : 0 , xhigh ? xhigh : 1 ],
+        y: [ ylow ? ylow : 0 , yhigh ? yhigh : 1 ]
+      }
+
+
+      this.motor = setInterval(this.tick.bind(this), 300);
+      
+
+    }
+
+    Gesture.prototype.setBuffer = function(buff) {
+      this.buffer = buff ? buff : this.buffer
+      
+    }
+
+    Gesture.prototype.kill = function() {
+      clearInterval(this.motor)
+    }
+
+    Gesture.prototype.tick = function() {
+
+      this.bufferIndex += this.speed
+      this.bufferIndex %= this.limit
+
+
+      var interval = this.limit / this.buffer.length;
+
+      var lowerIndex = Math.floor(this.bufferIndex/interval)
+      var upperIndex = lowerIndex + 1
+
+      upperIndex %= this.buffer.length
+
+      var interpDistance = (this.bufferIndex - lowerIndex*interval) / interval
+
+      var x = nx.interp(interpDistance, this.buffer[lowerIndex][0],this.buffer[upperIndex][0])
+      var y = nx.interp(interpDistance, this.buffer[lowerIndex][1],this.buffer[upperIndex][1])
+
+      x = nx.scale(x,0,1,this.scale.x[0],this.scale.x[1])
+      y = nx.scale(y,0,1,this.scale.y[0],this.scale.y[1])
+
+      this.cb(x,y)
+
+    }
